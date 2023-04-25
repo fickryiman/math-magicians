c9c94564f1a62164cf731368b85e63a05c390082
Fickry Iman <fickry.iman@gmail.com>
2023-04-25 22:46:13 +0700

    add calculator component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello World!</p>
-      </header>
-    </div>
+    <Calculator />
   );
 }
 
